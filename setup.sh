5302b0e2b6e38cd4ba1045eeaf7a35093af6ebd6
Julio Napurí <julionc@gmail.com>
2022-04-18 12:02:45 -0500

    feat: add setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as sudo"
+   echo "-------------------------------------------"
+   echo "This is what you should type into terminal:"
+   echo "sudo bash setup.sh"
+   exit 1
+fi
+
+sudo ansible-playbook -c local -i localhost,telefonica,telefonica-mb playbook.yml
